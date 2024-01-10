4cbbc61cd721bc8c13af0a910d17cf3a1fe90e6e
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-01-10 21:25:13 +0000

    Wed, Jan 10, 2024, 9:25 PM +00:00

--- hello.c
+++ hello.c
@@ -4,5 +4,8 @@
 int main(void)
 {
     string name = get_string("What is your name? ");
+    int age = get_int("How old r u ? ");
+    int phone_number = get_long("What's your Phone Number? ");
+    
     printf("Hello, %s\n", name);
 }
