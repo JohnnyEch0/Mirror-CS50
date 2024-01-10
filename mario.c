6d1dc48c6de0ee52fb7d0902b6cbb5494d12cda1
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-01-10 21:52:32 +0000

    Wed, Jan 10, 2024, 9:52 PM +00:00

--- mario.c
+++ mario.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <cs50.h>
 
-void print_row(int length)
+void print_row(int length);
 
 
 int main(void)
 {
     int height = get_int("Height: ");
-    for (i = 0; i > height, i++)
+    for (int i = 0; i > height, i++)
     {
         print_row(height);
 
