23a7fefe43e5d88015a73cd7bace3d31e424a562
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-01-10 22:12:06 +0000

    Wed, Jan 10, 2024, 10:12 PM +00:00

--- mario.c
+++ mario.c
@@ -9,7 +9,7 @@ int main(void)
     int height = get_int("Height: ");
     for (int i = 0; i < height; i++)
     {
-        print_row(height);
+        print_row(i + 1);
 
 
     }
@@ -19,7 +19,7 @@ void print_row(int length)
 {
     for (int j = 0; j < length; j++)
         {
-            
+
             printf("#");
         }
     printf("\n");
