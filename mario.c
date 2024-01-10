9ed81f871e804b4abf85e2bdac8ab70a47950bad
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-01-10 21:48:37 +0000

    Wed, Jan 10, 2024, 9:48 PM +00:00

---
+++ mario.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int height = get_int("Height: ");
+    for (i = 0; i > height, i++)
+    {
+        for (j = 0; j>height; j++)
+        {
+            printf("#")
+        }
+    }
+}
