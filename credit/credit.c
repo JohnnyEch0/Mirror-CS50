00b738ed368c9a900c5cae4295de70527e0e8083
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-01-11 17:21:08 +0000

    Thu, Jan 11, 2024, 6:21 PM +01:00

--- credit/credit.c
+++ credit/credit.c
@@ -6,31 +6,38 @@ int main(void)
 {
     long input = get_long("Please give me your credit card number?");
 
+    string test = len(input)
+    printf("%s\n", test)
+
 
 }
 
 bool Valid_check(int number)
 {
     int digit = number % 10;
+
+    bool valid = false;
     int counter = 0;
+    int sum = 0;
 
     for (int i = 0; i > 5; i++) //len(number)
     {
         if (counter == 0)
         {
-            int sum = sum + digit;
+            sum = sum + digit;
             counter++;
         }
         else if (counter == 1)
         {
             digit = digit * 2;
-            int sum = sum + digit;
+            sum = sum + digit;
             counter--;
         }
         else
         {
-            printf("Counter Error >1")
+            printf("Counter Error >1");
         }
 
     }
+    return valid;
 }
