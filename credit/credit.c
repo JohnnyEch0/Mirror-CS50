f956282746ef13439a5409c80f59dc9f6904e70a
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-01-11 17:15:40 +0000

    Thu, Jan 11, 2024, 6:15 PM +01:00

--- credit/credit.c
+++ credit/credit.c
@@ -11,10 +11,26 @@ int main(void)
 
 bool Valid_check(int number)
 {
-    number % = int digit
+    int digit = number % 10;
+    int counter = 0;
 
-    for (int i = 0; i>len(number); i++)
+    for (int i = 0; i > 5; i++) //len(number)
     {
-        if i 
+        if (counter == 0)
+        {
+            int sum = sum + digit;
+            counter++;
+        }
+        else if (counter == 1)
+        {
+            digit = digit * 2;
+            int sum = sum + digit;
+            counter--;
+        }
+        else
+        {
+            printf("Counter Error >1")
+        }
+
     }
 }
