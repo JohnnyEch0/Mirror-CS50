0c4f30887751423ef7b4a81e1f1c5939eaab5bc7
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-01-11 09:19:11 +0000

    Thu, Jan 11, 2024, 10:19 AM +01:00

---
+++ mario-more/mario_more_2.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <cs50.h>
+
+void print_row(int length, int user_input);
+
+
+
+int main(void)
+{
+    int height = get_int("Height: ");
+    for (int i = 0; i < height; i++)
+    {
+        print_row(i + 1, height);
+
+
+    }
+}
+
+void print_row(int length, int user_input)
+{
+    // print a number of spaces before the #, so that they are centered
+    for (int k = 5 - length; k > 0; k--)
+    {
+        printf(" ");
+    }
+
+    //print the first #'s
+    for (int j = 0; j < length; j++)
+        {
+
+            printf("#");
+
+        }
+
+    // print the space in between
+    printf("  ");
+
+    //print the second #`s
+    for (int j = 0; j < length; j++)
+        {
+
+            printf("#");
+
+        }
+    printf("\n");
+}
+
---
+++ mario-more/note.c
@@ -0,0 +1,6 @@
+User input * 2 + 2 = length of every line
+
+First Loop is for the lines
+
+print line function should get i and User Input
+User Input-i could be the number of " " needed before the #, while i will be the number of #
