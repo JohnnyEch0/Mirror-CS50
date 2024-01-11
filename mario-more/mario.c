c047067d0879d69e236ac19499918ebf9269bd71
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-01-11 16:34:29 +0000

    Thu, Jan 11, 2024, 5:34 PM +01:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -4,7 +4,6 @@
 void print_row(int length_i, int user_input);
 
 
-
 int main(void)
 {
     int height;
@@ -18,36 +17,26 @@ int main(void)
     for (int i = 0; i < height; i++)
     {
         print_row(i + 1, height);
-
-
     }
 }
 
 void print_row(int length_i, int user_input)
 {
-    // print a number of spaces before the #, so that they are centered
     for (int k = user_input - length_i; k > 0; k--)
     {
         printf(" ");
     }
 
-    //print the first #'s
     for (int j = 0; j < length_i; j++)
         {
-
             printf("#");
-
         }
 
-    // print the space in between
     printf("  ");
 
-    //print the second #`s
     for (int j = 0; j < length_i; j++)
         {
-
             printf("#");
-
         }
     printf("\n");
 }
