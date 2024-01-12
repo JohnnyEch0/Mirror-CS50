c377d0fcc505c00746acada8e27b6b897ee2ea3a
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-01-12 09:29:08 +0000

    Fri, Jan 12, 2024, 10:29 AM +01:00

--- cash/cash.c
+++ cash/cash.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <cs50.h>
 
+int coins_needed(change);
+
+
 int main(void)
 {
     int input;
@@ -9,7 +12,43 @@ int main(void)
         input = get_int("How much change? (int>0)");
     }
     while (input < 1);
-    printf("%i\n", input);
+    // printf("%i\n", input);
+    int output = coins_needed(input);
+    print("%i coins needed", output);
 
+
+}
+
+int coins_needed(change)
+{
+    int counter = 0
+
+    do
+    {
+        if change >= 25
+        {
+        change - 25;
+        counter++;
+        }
+        else if change >= 10
+        {
+            change - 10;
+            counter++;
+        }
+        else if change >= 5;
+        {
+            change - 5;
+            counter++;
+        }
+        else if change >= 1;
+        {
+            change - 1;
+            counter++;
+        }
+        else
+            printf("Change calc error")
+    }
+    while (change > 0);
+    return counter;
 
 }
