dab7a8bc29b365d39358795276af9e94b3b0f174
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-01-12 09:35:39 +0000

    Fri, Jan 12, 2024, 10:35 AM +01:00

--- cash/cash.c
+++ cash/cash.c
@@ -9,7 +9,7 @@ int main(void)
     int input;
     do
     {
-        input = get_int("How much change? (int>0)");
+        input = get_int("Change owed:");
     }
     while (input < 1);
     // printf("%i\n", input);
