5e560a2d9f654f8532d7ba5b0bcb78a5f69779ae
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-01-14 23:51:51 +0000

    Mon, Jan 15, 2024, 12:51 AM +01:00

--- Readability/tests.c
+++ Readability/tests.c
@@ -20,7 +20,7 @@ int main(void)
 
     }
 
-    float av_word_len = strlen(text) / (spaces+1);
+    float av_word_len = float(strlen(text)-spaces) / (spaces+1);
 
     // printf("there are %i spaces in this text, which should tell me that it has %i words\n", spaces, spaces + 1);
     printf("%f is the average word length\n", av_word_len);
