b3572255e3633e6edcc2ae4b704bf5942cc457fd
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-01-14 23:21:45 +0000

    Mon, Jan 15, 2024, 12:21 AM +01:00

--- Readability/tests.c
+++ Readability/tests.c
@@ -1,7 +1,14 @@
-
+#include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
-    text = getstring("Please give me the text to evaluate:");
-    
+    string text = getstring("Please give me the text to evaluate:");
+    for (int i = 0, int n = getlen(text); i < n; i++)
+    {
+        if int(text[i]) == 32
+        {
+            printf("there is a space as position %i", i)
+        }
+    }
 }
