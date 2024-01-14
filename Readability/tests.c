05e1ef0c52c369f8bc265d14a8347c63ff9fcc8e
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-01-14 23:44:44 +0000

    Mon, Jan 15, 2024, 12:44 AM +01:00

--- Readability/tests.c
+++ Readability/tests.c
@@ -8,11 +8,15 @@ int main(void)
     for (int i = 0, n = strlen(text); i < n; i++)
     {
         int letter = text[i];
+        int spaces = 0;
         if (letter == 32)
         {
+            spaces++;
+
             printf("there is a space as position %i\n", i);
-            printf("%s", text[]);
-            //should i just count the spaces?
+
+            //should i just count the spaces? is the number of words always the number of spaces+1?
         }
+        printf("there are %i spaces in this text, which should tell me that it has %i words", spaces, spaces + 1);
     }
 }
