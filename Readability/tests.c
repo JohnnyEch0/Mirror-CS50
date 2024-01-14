c9ee60d811f66ece9e5c480c6bf8783cae3eb70a
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-01-14 23:49:58 +0000

    Mon, Jan 15, 2024, 12:49 AM +01:00

--- Readability/tests.c
+++ Readability/tests.c
@@ -5,18 +5,23 @@
 int main(void)
 {
     string text = get_string("Please give me the text to evaluate:");
+    int spaces = 0;
+
     for (int i = 0, n = strlen(text); i < n; i++)
     {
         int letter = text[i];
-        int spaces = 0;
+
         if (letter == 32)
         {
             spaces++;
-
-            printf("there is a space as position %i\n", i);
-
+            // printf("there is a space as position %i\n", i);
             //should i just count the spaces? is the number of words always the number of spaces+1?
         }
-        printf("there are %i spaces in this text, which should tell me that it has %i words", spaces, spaces + 1);
+
     }
+
+    float av_word_len = strlen(text) / (spaces+1);
+
+    // printf("there are %i spaces in this text, which should tell me that it has %i words\n", spaces, spaces + 1);
+    printf("%f is the average word length\n", av_word_len);
 }
