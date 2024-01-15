2d3067327b86ab6e2cf48b2517840c6ce160d202
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-01-15 10:01:23 +0000

    Mon, Jan 15, 2024, 11:01 AM +01:00

--- Readability/tests.c
+++ Readability/tests.c
@@ -7,9 +7,9 @@ int main(void)
 {
     string text = get_string("Please give me the text to evaluate:");
     int spaces = 0;
-    int non_let_spa = 0; // was punc_marks
+    int non_let_spa = 0; 
     int txt_len = strlen(text);
-    int punc = 0; //was non_punc
+    int punc = 0;
 
     for (int i = 0, n = txt_len; i < n; i++)
     {
@@ -32,14 +32,11 @@ int main(void)
 
     float non_letters = spaces+non_let_spa;
     int word_count = spaces+1;
-    // int punc = non_let_spa - non_punc; //was non_punc
 
 
     float av_word_len = (float)(txt_len-non_letters) / word_count * 100;
     float sen_p_words = (float)punc / word_count * 100;
 
-    // printf("%f is the average words per 100 words (kindoff haha)\n", av_word_len);
-    // printf("there are %f sentences per 100 words", sen_p_words);
 
     int index = round(0.0588 * av_word_len - 0.296 * sen_p_words - 15.8);
 
