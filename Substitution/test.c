62ce2ab7136e815d3a49edbbf2b6bc8370a7646f
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-01-15 11:09:35 +0000

    Mon, Jan 15, 2024, 12:09 PM +01:00

--- Substitution/test.c
+++ Substitution/test.c
@@ -1,9 +1,20 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 int main(string key)
 {
     validate_input(key);
 }
 
+void validate_input(key)
+{
+    int len = strlen(key);
+    if len = 26
+    {
+        printf("len verified");
+    }
+    else
+        printf("input must be 26 letters");
+}
