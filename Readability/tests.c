53e1fc92b867f4699a76f8c11f49832990e67044
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-01-15 10:32:57 +0000

    Mon, Jan 15, 2024, 11:32 AM +01:00

--- Readability/tests.c
+++ Readability/tests.c
@@ -4,25 +4,14 @@
 #include <math.h>
 
 int calc_index(string input);
+void print_answer(int p_index);
+
 
 int main(void)
 {
     string text = get_string("Please give me the text to evaluate:");
-
     int index = calc_index(text);
-
-
-
-
-
-
-
-
-
-    // this can be a seperate function
-
-    printf("Level: %i\n", index);
-
+    print_answer(index);
 }
 
 
@@ -41,13 +30,12 @@ int calc_index(string input)
         {
             spaces++;
         }
-        else if (letter <= 64)
+        else if (letter <= 64) // we need to check if the char is ?,! or .
         {
-            // we need to check if the char is ?,! or .
             non_let_spa++;
             if (letter == 33 || letter == 63 || letter == 46)
             {
-                punc++; //was non_punc
+                punc++;
             }
         }
     }
@@ -55,13 +43,25 @@ int calc_index(string input)
     float non_letters = spaces+non_let_spa;
     int word_count = spaces+1;
 
-
     float av_word_len = (float)(txt_len-non_letters) / word_count * 100;
     float sen_p_words = (float)punc / word_count * 100;
 
-
-
     int index = round(0.0588 * av_word_len - 0.296 * sen_p_words - 15.8);
     return index;
+}
 
+void print_answer(int p_index)
+{
+    if (p_index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (p_index > 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %i\n", p_index);
+    }
 }
