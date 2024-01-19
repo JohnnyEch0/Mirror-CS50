daefab28940bc097301f98542554077e675343f5
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-01-19 14:25:33 +0000

    Fri, Jan 19, 2024, 3:25 PM +01:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -182,7 +182,7 @@ void sort_pairs(void)
             if (preferences[i][j] < preferences[i][j+1])
             {
                 // int mem = preferences[i][j];
-                pair mem = 
+                pair mem_pair = {}
             }
         }
     }
