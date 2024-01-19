3e02da0eb1ec5fd18f64a31b7499a10301cc508c
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-01-19 12:31:56 +0000

    Fri, Jan 19, 2024, 1:31 PM +01:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -119,10 +119,10 @@ void record_preferences(int ranks[])
 {
     // TODO
     // ranks[0,1,2] for candidate
+    // loop through the fckin ranks
     for (int i = 0; i < candidate_count; i++)
     {
-        // i = 0 --> candidate 1, 2 ...  - but it ccould also be the ranks
-        // ranks[i] equals the number of the candidate at rank 1
+        // each higher rank is 
         for (int j = 0; j < candidate_count-i; j++)
             preferences[rank[i]][rank[i+x]]++;
         // candidates[ranks[i]]
