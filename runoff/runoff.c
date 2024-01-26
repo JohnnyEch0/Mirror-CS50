a5fc0ee953d816f513180bd850a135486a469195
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-01-26 11:21:49 +0000

    Fri, Jan 26, 2024, 12:21 PM +01:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -191,10 +191,17 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
+// temp variable for the highest resuolt
+
+    winner_1 = candidadates[0]
     // check if there is a winner
     for (int i = 0; i < candidate_count; i++)
     {
-        
+        if (winner_1.value < candidate[i].value):
+        {
+            
+        }
+
     }
     return false;
 }
