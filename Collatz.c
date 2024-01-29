8128df6086694a0bb13916ce53a6f461d23755d0
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-01-29 20:19:53 +0000

    Mon, Jan 29, 2024, 9:19 PM +01:00

--- Collatz.c
+++ Collatz.c
@@ -1,8 +1,19 @@
+void collatz(x)
+{
+    if x == 1
+        return 1
+
+    else
+    {
+        if x
+    }
+}
+
 
 void main(argc, argv)
 {
     if (argc != 2)
         return 1
 
-    argv[1]
+    collatz(argv[1])
 }
