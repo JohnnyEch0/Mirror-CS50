658c6ffd4efc1485099d5e0b6851c3a0dc35531a
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-01-29 20:39:21 +0000

    Mon, Jan 29, 2024, 9:39 PM +01:00

--- Collatz.c
+++ Collatz.c
@@ -10,13 +10,13 @@ void collatz(int x, int y)
     else if (x % 2 == 0)
     {
         y++;
-        return collatz(x/2, y);
+        collatz(x/2, y);
     }
 
     else
     {
         y++;
-        return collatz(3*x + 1, y);
+        collatz(3*x + 1, y);
     }
 
 }
@@ -24,9 +24,6 @@ void collatz(int x, int y)
 
 int main(int argc, string argv[])
 {
-    if (int(argv[1]) != 2)
-        return 1;
-
-
-    collatz(argv[1], 0);
+    int x = (argv[1]);
+    collatz(x, 0);
 }
