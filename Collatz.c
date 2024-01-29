9ef2e68d22a3846d6ad926c1a2e5411cc9b4fd5d
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-01-29 20:27:40 +0000

    Mon, Jan 29, 2024, 9:27 PM +01:00

--- Collatz.c
+++ Collatz.c
@@ -1,19 +1,21 @@
 void collatz(int x)
 {
     if x == 1
-        return 1
+        return 1;
 
+    elif (argc % 2 == 0)
+        return collatz(x/2);
     else
-    {
-        if x
-    }
+        return collatz(3*x + 1);
+    printf("%i steps to Collatz %i",)
 }
 
 
 void main(argc, argv)
 {
     if (argc != 2)
-        return 1
+        return 1;
 
-    collatz(argv[1])
+
+    collatz(argv[1]);
 }
