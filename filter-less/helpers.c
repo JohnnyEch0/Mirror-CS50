c2b3fd27e92b172554cb3752ea437e4de1cd5e5a
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-02-03 16:29:30 +0000

    Sat, Feb 3, 2024, 5:29 PM +01:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -66,5 +66,12 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < heigth; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            
+        }
+    }
     return;
 }
