ac1e27e33fb79d5add78f4924645f39f2bafb51c
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-02-04 13:35:38 +0000

    Sun, Feb 4, 2024, 2:35 PM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 
     // create the buffer
     uint8_t buffer[512];
-    uint8_t header[3]; //3.5?
+    uint4_t header[7]; //3.5?
 
     while (fread(&buffer, 1, 512, input) == 512)
     {
