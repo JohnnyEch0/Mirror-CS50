7992427ed5ea5024bd2b370b088de5a7fe2827fd
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-02-04 14:31:20 +0000

    Sun, Feb 4, 2024, 3:31 PM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -31,21 +31,7 @@ int main(int argc, char *argv[])
         if (buffer[0] == 0xFF && buffer[1] == 0xD8 && buffer[2] == 0xFF && buffer[3] >= 0xE0)
         {
             count++;
-            if (count > 9)
-            {
-                ;
-            }
-            for (int i = 0; i < 3; i++)
-            {
-                if (count > 9)
-                {
-                    out = "0" + (c) 
-                }
-                out[i] = 0;
-            }
-
-            printf("%s\n", out);
-
+            fprintf(stdout, "%.3d\n", count);
 
             // printf("image found\n");
 
