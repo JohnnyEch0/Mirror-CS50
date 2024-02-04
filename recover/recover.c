b199f8a80a976962ded0fd429a36d10b0b3aadc7
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-02-04 13:32:46 +0000

    Sun, Feb 4, 2024, 2:32 PM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
     {
         printf("block found\n");
         // if the block starts with 0xff 0xd8 0xff 0xe0 (last zero might be different)
-        // start a new file
+        // --> start a new file
         //
 
     }
