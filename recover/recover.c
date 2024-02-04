8cd4f9980877916df1267ad6f24e59ad28bf4623
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-02-04 13:49:28 +0000

    Sun, Feb 4, 2024, 2:49 PM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -21,14 +21,21 @@ int main(int argc, char *argv[])
 
     // create the buffer
     uint8_t buffer[512];
+    int count = -1;
+    char *out = "000"
+
 
     while (fread(&buffer, 1, 512, input) == 512)
     {
         // printf("block found\n");
         if (buffer[0] == 0xFF && buffer[1] == 0xD8 && buffer[2] == 0xFF && buffer[3] >= 0xE0)
         {
+            count++;
+            out = 
+
+
             // printf("image found\n");
-            
+
         }
         // if the block starts with 0xff 0xd8 0xff 0xe0 (last zero might be different)
         // --> start a new file
