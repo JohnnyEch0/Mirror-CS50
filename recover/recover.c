f4949acb2a9921c8d0849f4b37234ab6f3770d95
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-02-04 15:30:10 +0000

    Sun, Feb 4, 2024, 4:30 PM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -19,28 +19,36 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    // create the buffer
+    // create the buffer, count
     uint8_t buffer[512];
     int count = -1;
+
+    // malloc the out --> filename
     char *out = malloc(8);
+
+    // initialize the ouput --> file
     FILE *output = NULL;
 
-
+    // recover .jpg images from the input, naming them ###
     while (fread(&buffer, 1, 512, input) == 512)
     {
         // if a new file is found, create a new name
         if (buffer[0] == 0xFF && buffer[1] == 0xD8 && buffer[2] == 0xFF && buffer[3] >= 0xE0)
         {
+            // close previous file, if there is one
             if (output != NULL)
                 fclose(output);
+
+            // create the new filename
             count++;
             sprintf(out, "%.3d.jpg", count);
-            // printf("%s\n", out);
+
+            // open the new file for writing
             output = fopen(out, "w");
         }
+        //write to the file, if there is an output
         if (output != NULL)
             fwrite(&buffer, sizeof(buffer), 1, output);
-
     }
 
     // close files
