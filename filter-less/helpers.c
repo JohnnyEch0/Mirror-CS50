a969c313502ef01f4d99044af0fa49192069d032
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-02-04 11:06:55 +0000

    Sun, Feb 4, 2024, 12:06 PM +01:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -9,7 +9,8 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            BYTE gray_pixel = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3);
+            float nu_value = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3;
+            BYTE gray_pixel = round(nu_value);
             image[i][j].rgbtBlue = gray_pixel;
             image[i][j].rgbtGreen = gray_pixel;
             image[i][j].rgbtRed = gray_pixel;
@@ -26,7 +27,8 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             // First, conver to grayscale
-            BYTE gray_pixel = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3);
+            float nu_value = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3;
+            BYTE gray_pixel = round(nu_value);
 
             // increase red and green values
             int SCALE = 20;
