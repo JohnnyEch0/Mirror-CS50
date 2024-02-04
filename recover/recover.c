50db3def198d25a4641f5402719773ab01679d9c
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-02-04 13:18:39 +0000

    Sun, Feb 4, 2024, 2:18 PM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -3,9 +3,17 @@
 
 int main(int argc, char *argv[])
 {
+    // check 4 correct Usage
     if (argc != 2)
         printf("Usage: ./recover [image to be recovered]\n");
         return 1;
-    
+
+    // Open file
+    FILE *input = fopen(argv[1], "r");
+    if (input == NULL)
+    {
+        printf("Could not open file.\n");
+        return 2;
+    }
 
 }
