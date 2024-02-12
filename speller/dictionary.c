2b485f2d2bb4c93f82efde6ede0c4e5811de72b2
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-02-12 14:32:21 +0000

    Mon, Feb 12, 2024, 3:32 PM +01:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -47,8 +47,9 @@ bool load(const char *dictionary)
             // n->next = NULL;
         // find n for table[n] / hash the word(?)
             // hash returns the bucket- or the index for the hashlist
-            // now create a Node with char[LENGTH+1]
-                // check if struct->next should be NULL or prev
+            // set new node pointer to the first element in the linked list
+                // now have the list pointer ponint to new node
+
         // if there is a node there
             // node->next = the node already there
             // what if there are multiple nodes?
