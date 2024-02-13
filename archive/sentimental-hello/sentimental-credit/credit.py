a6891248ba4219a953551935c5cca6a31f2d75a1
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-02-13 23:49:32 +0000

    Wed, Feb 14, 2024, 12:49 AM +01:00

--- archive/sentimental-hello/sentimental-credit/credit.py
+++ archive/sentimental-hello/sentimental-credit/credit.py
@@ -14,13 +14,13 @@ for i, digit in enumerate(number):
             mult = str(digit_3)
             for j, digit_j in enumerate(mult):
                 mult2 += int(digit_j)
-                print("case 0: digit to mult", digit_j, "from", digit)
+                # print("case 0: digit to mult", digit_j, "from", digit)
 
         else:
             add += int(digit)
-            print("case 0: first digit to add", digit)
+            # print("case 0: first digit to add", digit)
             digit2 = number[-1]
-            print("case 0: last digit to add", digit2)
+            # print("case 0: last digit to add", digit2)
             add += int(digit2)
 
         # print("Case 0:", add, mult2)
@@ -31,16 +31,16 @@ for i, digit in enumerate(number):
         mult = str(digit)
         for j, digit_j in enumerate(mult):
                 mult2 += int(digit_j)
-                print("digit to mult2", digit_j, "from", digit)
+                # print("digit to mult2", digit_j, "from", digit)
         if (i*2) + 1 < length:
             digit2 = number[(-2*i) - 1]
-            print("digit to add", digit2)
+            # print("digit to add", digit2)
             add += int(digit2)
 
 
-print(add, mult2)
+# print(add, mult2)
 
-if mult2 + add != 20:
+if (mult2 + add) % 10 != 0:
     print("INVALID")
 elif number[0] == 4:
     print("VISA")
