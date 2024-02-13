1afc27fc68281d67b2a078b32a28e9d269970e1d
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-02-13 22:40:51 +0000

    Tue, Feb 13, 2024, 11:40 PM +01:00

--- archive/sentimental-hello/sentimental-credit/credit.py
+++ archive/sentimental-hello/sentimental-credit/credit.py
@@ -3,13 +3,17 @@ from cs50 import get_string
 number = get_string("Number: ")
 mult = ""
 add = 0
+mult2 = 0
+
 for i, digit in enumerate(number):
     if i % 2 == 0:
-        mult += str((int(digit*2))
-        # print(mult, i)
+        mult = str((int(digit*2)))
+        for j, digit in enumerate(mult):
+
+            mult2 += int(digit)
+            print(mult, mult2)
     else:
         add += int(digit)
-mult2 = 0
-for i, digit in enumerate(mult):
-    mult2 += int(digit)
-    print(i, mult2)
+
+
+    # print(i, mult2)
