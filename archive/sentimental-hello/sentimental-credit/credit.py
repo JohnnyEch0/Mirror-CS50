980878b48c6dcf0ee3447805a626fdc81c87a396
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-02-13 22:31:51 +0000

    Tue, Feb 13, 2024, 11:31 PM +01:00

--- archive/sentimental-hello/sentimental-credit/credit.py
+++ archive/sentimental-hello/sentimental-credit/credit.py
@@ -6,8 +6,9 @@ add = 0
 for i, digit in enumerate(number):
     if i % 2 == 0:
         mult += int(digit) * 2
-        print(mult, i)
+        # print(mult, i)
     else:
         add += int(digit)
+
 
     # print(i)
