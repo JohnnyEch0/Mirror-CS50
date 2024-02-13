2d8e044a0fe5e08a38093459fe04bc900e0b2fb8
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-02-13 22:04:47 +0000

    Tue, Feb 13, 2024, 11:04 PM +01:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -3,6 +3,8 @@ while True:
     x = int(input("Height:"))
     if x < 9 and x > 0:
         break
+    else:
+        print("Usage: int 1-8")
 
 for i in range(x):
     line = str((" " * (x - i-1)) + ("#" * (i+1)) + "  " + ("#" * (i+1)) + (" " * (x - i-1)))
