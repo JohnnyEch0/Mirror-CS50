a60ad47a7384fec540641af56d032772eb0b29fa
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-02-14 23:40:58 +0000

    Thu, Feb 15, 2024, 12:40 AM +01:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,8 +1,9 @@
 import cs50
 text = cs50.get_string("Text: ")
 sentences = 0
-words = 0
+words = 1 #spaces+1
 letters = 0
+length = len(text)
 
 for c in text:
     if c == "." or c == "?" or c == "!":
@@ -12,7 +13,8 @@ for c in text:
     else:
         letters +=1
 
-cl_index = 
+av_word_length = (length - words - sentences)
+cl_index =
 
 # 0.0588 * L - 0.296 * S - 15.8, where L is the average number of letters per 100 words in the text, and S is the average number of sentences per 100 words in the text.
 print(sentences)
