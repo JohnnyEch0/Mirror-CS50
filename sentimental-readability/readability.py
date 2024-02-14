73ce621cd2713443d104592fd5851a8fcb84d9ee
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-02-14 23:46:59 +0000

    Thu, Feb 15, 2024, 12:46 AM +01:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,7 +1,7 @@
 import cs50
 text = cs50.get_string("Text: ")
 sentences = 0
-words = 1 #spaces+1
+spaces = 0
 letters = 0
 length = len(text)
 
@@ -9,12 +9,22 @@ for c in text:
     if c == "." or c == "?" or c == "!":
         sentences +=1
     elif c == " ":
-        words += 1
+        spaces += 1
     else:
         letters +=1
 
-av_word_length = (length - words - sentences)
-cl_index =
+words = spaces+1
+
+av_word_length = (length - spaces - sentences) / words * 100
+sen_p_words = sentences / words * 100
+cl_index = round(0.0588 * av_word_length - 0.296 * sen_p_words - 15.8)
+
+if cl_index < 1:
+    print("Before Grade 1")
+elif cl_index > 16:
+    print("Grade 16+")
+else:
+    print(f"Grade {cl_index}")
 
 # 0.0588 * L - 0.296 * S - 15.8, where L is the average number of letters per 100 words in the text, and S is the average number of sentences per 100 words in the text.
-print(sentences)
+
