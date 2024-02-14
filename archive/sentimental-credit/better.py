9cde280620afb520d2f4959dd3cdab0a176ee6e6
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-02-14 10:53:55 +0000

    Wed, Feb 14, 2024, 11:53 AM +01:00

--- archive/sentimental-credit/better.py
+++ archive/sentimental-credit/better.py
@@ -1,4 +1,8 @@
+import re
 
+visa_pattern = r"^4\d{12}(?:\d{3})?$"
+amex_pattern = r"^3[47]\d{13}$"
+mastercard_pattern = r"^5[1-5]\d{14}$"
 
 def luhn_valid(number):
     digits = [int(d) for d in number[::-1]]
@@ -17,5 +21,14 @@ def luhn_valid(number):
 
 
 number = input("Number: ")
-print(luhn_valid(number))
+if luhn_valid(number):
+    if re.match(visa_pattern, number):
+        print("VISA")
+    elif re.match(amex_pattern, number):
+        print("AMEX")
+    elif re.match(mastercard_pattern, number):
+        print("MASTERCARD")
+    else:
+        print("INVALID")
+
 
