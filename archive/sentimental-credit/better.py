759193e6467122ffbc121c6661c4873671677f62
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-02-14 10:46:30 +0000

    Wed, Feb 14, 2024, 11:46 AM +01:00

---
+++ archive/sentimental-credit/better.py
@@ -0,0 +1,21 @@
+
+
+def luhn_valid(number):
+    digits = [int(d) for d in number[::-1]]
+    # :: omits start and end values, so uses entire string
+    # -1 iterates in reverse
+
+    checksum = 0
+    for i, digit in enumerate(digits):
+        if digit % 2 == 0:
+            digit *= 2
+            if digit > 9:
+                # clever trick, if digit*2 is more then one digit, substract 9 to get quersum
+                digit -= 9
+        checksum += digit
+    return checksum % 10 == 0
+
+
+number = input("Number: ")
+print(luhn_valid(number))
+
