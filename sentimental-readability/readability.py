92416d356776e1fc8644fe4bbcaac9d220a52f7c
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-02-15 01:08:12 +0000

    Thu, Feb 15, 2024, 2:08 AM +01:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -32,3 +32,7 @@ else:
 
 # 0.0588 * L - 0.296 * S - 15.8, where L is the average number of letters per 100 words in the text, and S is the average number of sentences per 100 words in the text.
 
+# 62 until bank
+# 22 until to do
+# 53 until reading
+# 
