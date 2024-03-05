f10014f2d2fe128bd628036b706b03811cc1eae7
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-03-05 11:36:03 +0000

    Tue, Mar 5, 2024, 12:36 PM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -3,7 +3,10 @@
 -- Check the Crime Scene reports for matching Reports
 SELECT * FROM crime_scene_reports WHERE street = 'Humphrey Street' AND month = 7 AND day = 28;
 
--- all witnesses mention the bakery
+-- all 3 witnesses mention the bakery
 -- Time: 10:15am
+-- also there was a littering
 
-SELECT * FROM bakery_security_logs WHERE 
+-- read the bakeries sec logs at the given time
+SELECT activity, license_plate FROM bakery_security_logs WHERE
+    month = 7 AND day = 28 AND year = 2023;
