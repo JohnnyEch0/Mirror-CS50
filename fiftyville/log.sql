8e69627a09c2151bda9741985cf8e31da64cd89f
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-03-05 11:59:31 +0000

    Tue, Mar 5, 2024, 12:59 PM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -19,3 +19,10 @@ SELECT name, transcript FROM interviews WHERE
     month = 7 AND day = 28 AND year = 2023;
 
 -- switch to .output stdout
+
+
+-- Eugene: I don't know the thief's name, but it was someone I recognized. Earlier this morning, before I arrived at Emma's bakery,
+-- I was walking by the ATM on Leggett Street and saw the thief there withdrawing some money.
+
+SELECT account_number, transaction_type, amount FROM atm_transactions WHERE
+    month = 7 AND day = 28 AND year = 2023 AND atm_location = 'Leggett Street';
