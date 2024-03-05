316e31ed239099656d22eb7b35fcbd5485838645
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-03-05 11:31:33 +0000

    Tue, Mar 5, 2024, 12:31 PM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,3 +1,4 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
-SELECT * FROM crime_scene_reports WHERE 
+-- Check the Crime Scene reports for matching Reports
+SELECT * FROM crime_scene_reports WHERE street = 'Humphrey Street' AND month = 7 AND day = 28;
