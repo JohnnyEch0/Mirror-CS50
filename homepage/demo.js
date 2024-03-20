539232b2931bca9777075f2b4c42e94aa93691d6
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-03-20 12:32:59 +0000

    Wed, Mar 20, 2024, 1:32 PM +01:00

--- homepage/demo.js
+++ homepage/demo.js
@@ -1,32 +1,15 @@
 function ready() {
+    alert('ready ok');
     var site_index = [
-    'index.html',
-    '1_All_the.html',
-    '2_Accumulation.html',
-    '3_Caliban.html',
-    '4_Witchhunt.html',
-    '5_Colonization.html',
-    '6_Final.html'
-];
-
-    current=$(location).attr('pathname').substr($(location).attr('pathname').lastIndexOf("/")+1); // get current page: just last part - page name and extension
-    if($.inArray(current, caseList)!==-1) { //check if it is in array
-        index=$.inArray(current, caseList);
-    }
-    //navigate thorough pages...
-    $( ".next" ).click(function() {
-    if(index<caseList.length){
-    next=caseList[index+1];
-    window.location.href = next;
-    }
-    });
-
-    $( ".prev" ).click(function() {
-    if(index>=0){
-    prev=caseList[index-1];
-    window.location.href = prev;
-    }
-    });
+        'index.html',
+        '1_All_the.html',
+        '2_Accumulation.html',
+        '3_Caliban.html',
+        '4_Witchhunt.html',
+        '5_Colonization.html',
+        '6_Final.html'
+        ];
+}
 
     current=$(location).attr('pathname').substr($(location).attr('pathname').lastIndexOf("/")+1); // get current page: just last part - page name and extension
     if(current.inArray(current, site_index)!==-1) { //check if it is in array
@@ -34,6 +17,7 @@ function ready() {
     }
 
     function next() {
+        alert('ok');
         if(index<site_index.length){
         next=site_index[index+1];
         window.location.href = next;
