2d66520e016fd709f9309b7a5edd2a9b4764e1a0
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-03-26 10:49:40 +0000

    Tue, Mar 26, 2024, 11:49 AM +01:00

--- finance/app.py
+++ finance/app.py
@@ -117,7 +117,7 @@ def register():
         if not request.form.get("username"):
             return apology("must provide username", 403)
 
-        elif db.execute("SELECT username FROM users WHERE ? IN username", request.form.get("username")) != None:
+        elif db.execute("SELECT username FROM users WHERE ? IN users", request.form.get("username")) != None:
             return apology("Username already in use", 403)
 
 
