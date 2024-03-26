897f9b2d68ea69fa2efa99a63ce3eb9482282601
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-03-26 11:30:00 +0000

    Tue, Mar 26, 2024, 12:30 PM +01:00

--- finance/app.py
+++ finance/app.py
@@ -106,7 +106,9 @@ def logout():
 @login_required
 def quote():
     """Get stock quote."""
-    return apology("TODO")
+    return render_template("quote.html")
+
+    # return apology("TODO")
 
 
 @app.route("/register", methods=["GET", "POST"])
