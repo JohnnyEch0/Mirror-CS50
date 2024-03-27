58c3a204b3121e839237f02344b85d8c2602ff93
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-03-27 14:16:45 +0000

    Wed, Mar 27, 2024, 3:16 PM +01:00

--- finance/test.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime, date, time, timezone
-dt = datetime.now()
-format_day = f"{dt.year}-{dt.month}-{dt.day}"
-format_time= f"{dt.hour}:{dt.minute}:{dt.second}"
-
-print(format_time)
