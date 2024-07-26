2892e6ae23425823244e58e0f66dad7d708f0f2f
JohnnyEch0 <156070712+JohnnyEch0@users.noreply.github.com>
2024-07-26 06:17:43 +0000

    Fri, Jul 26, 2024, 8:17 AM +02:00

--- 50W/wiki/encyclopedia/views.py
+++ 50W/wiki/encyclopedia/views.py
@@ -12,31 +12,31 @@ def index(request):
 def entry(request, title):
     page = get_page(title)
     if page is not None:
-        return render(request,   
-                        "encyclopedia/entry.html", 
-                        {"title": title.capitalize(), 
+        return render(request,
+                        "encyclopedia/entry.html",
+                        {"title": title.capitalize(),
                         "text": MD().convert(page),
                         "title": title
                             }
-                        ) 
-    
+                        )
+
     else:
         return render(request, "encyclopedia/entry_error.html", {"message": "404 - Page not found..."}  )
-    
+
 def edit(request, title):
     page = get_page(title)
     if page is None:
         return render(request, "encyclopedia/entry_error.html", {"message": "404 - Page not found..."}  )
-    
+
     elif request.method == "GET":
         input_form = NewEntry({"title": title, "content": page})
-        
+
         return render(
-            request, "encyclopedia/edit.html", 
+            request, "encyclopedia/edit.html",
             {"form": input_form,
              "entry": title}
         )
-    
+
     elif request.method == "POST":
         input_form = NewEntry(request.POST)
         if input_form.is_valid():
@@ -46,34 +46,32 @@ def edit(request, title):
             util.save_entry(title, content)
             return redirect("entry", title=title)
 
-    
-
 def search(request):
     query = request.GET.get("q","").capitalize()
     page = get_page(query)
     if page is not None:
         return redirect("entry", title=query)
     substrings_found = set()
-    
+
     for entry in util.list_entries():
         # if query in entry.capitalize():
         lower_entry = entry.lower()
         if lower_entry.find(query.lower()) != -1:
             substrings_found.add(entry)
-    
+
     if substrings_found == set():
         return render(request, "encyclopedia/entry_error.html", {"message": "No such Page could be found :["}  )
     else:
         return render(request, "encyclopedia/search.html", {
             "entries": substrings_found
         })
-    
+
 def create_new_page(request):
     if request.method == "GET":
         return render(
             request, "encyclopedia/create_new_page.html", {"form": NewEntry()}
         )
-    
+
     input_form = NewEntry(request.POST)
     if input_form.is_valid():
         title = input_form.cleaned_data.get("title")
@@ -81,15 +79,14 @@ def create_new_page(request):
 
         if title.capitalize() in [entry.capitalize() for entry in util.list_entries()]:
             return render(request, "encyclopedia/entry_error.html", {"message": "This Article already exists..."} )
-        
+
         else:
             with open(f"entries/{title}.md", "w") as file:
-                file.write(f"# {title} \n")
                 file.write(content)
             return redirect("entry", title=title)
 
 def random(request):
-    
+
     page_name = choice(util.list_entries())
     return redirect("entry", title=page_name)
 
